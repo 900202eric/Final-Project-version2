3bb09fe19e1ebf7485b039b0641fb9cd2e75adde
Eric Huang <900202eric@gmail.com>
2020-01-10 04:59:26 +0800

    Initial Commit

---
+++ Final Project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Final Project
+//
+//  Created by 黃瑋辰 on 2020/1/10.
+//  Copyright © 2020 黃瑋辰. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
